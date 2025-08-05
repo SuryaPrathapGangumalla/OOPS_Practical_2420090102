5c01007e4f7cab8d28ce7837c3def4a22400eb50
SuryaPrathapGangumalla <2420090102@klh.edu.in>
2025-08-05 10:34:12 +0530

    Add files via upload

---
+++ calendarclass.java
@@ -0,0 +1,18 @@
+package myproject;
+import java.util.*;
+
+public class calendarclass {
+   public static void main(String[] args) {
+	   Calendar c=Calendar.getInstance();
+	   Date d=c.getTime();
+	   System.out.println(d);
+	   c.set(2023, c.SEPTEMBER, 13);
+	   int year=c.getWeekYear();
+	   System.out.println(year);
+	   int h=c.getFirstDayOfWeek();
+	   System.out.println(h);
+	   int k=c.getLeastMaximum(h);
+	   System.out.println(k);
+	   
+   }
+}
