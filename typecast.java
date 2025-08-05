85c6e0cfcd41999435299f65f313fa7c0ad8fc15
SuryaPrathapGangumalla <2420090102@klh.edu.in>
2025-08-05 10:25:01 +0530

    Add files via upload

---
+++ typecast.java
@@ -0,0 +1,10 @@
+package myproject;
+
+public class typecast {
+      public static void main(String[] args) {
+    	  double myDouble=9.78;
+    	  int myInt=(int)myDouble;
+    	  System.out.println(myDouble);
+    	  System.out.println(myInt);
+      }
+}
