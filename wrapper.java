31bbca34cc2e50377a76e42ad22439bff0f88e6d
SuryaPrathapGangumalla <2420090102@klh.edu.in>
2025-08-05 10:27:59 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,12 @@
+package myproject;
+
+public class wrapper {
+      public static void main(String[] args) {
+    	  Integer a=570;
+    	  Boolean b=true;
+    	  Double d=8.976;
+    	  System.out.println(a.toString().length());
+    	  System.out.println(d.toString().length());
+    	
+      }
+}
